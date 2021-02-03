45eeb9fb349fd6155c5c2c579ec1b744a06330d6
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2021-02-02 23:24:54 -0500

    add support application to cli

--- rtverse-cli/rtverse.js
+++ rtverse-cli/rtverse.js
@@ -4,6 +4,7 @@
 const blessed = require("blessed");
 const contrib = require("blessed-contrib");
 const RtverseAgent = require("rtverse-agent");
+const moment = require("moment");
 
 const agent = new RtverseAgent();
 
@@ -18,6 +19,11 @@ const grid = new contrib.grid({
   screen,
 });
 
+let extended = [];
+let selected = {
+  uuid: null,
+  type: null,
+};
 const tree = grid.set(0, 0, 1, 1, contrib.tree, {
   label: "Connected Agents",
 });
@@ -39,16 +45,94 @@ agent.on("agent/connected", (payload) => {
   renderData();
 });
 
+agent.on("agent/disconnected", (payload) => {
+  const { uuid } = payload.agent;
+
+  if (agents.has(uuid)) {
+    agents.delete(uuid);
+    agentMetrics.delete(uuid);
+  }
+
+  renderData();
+});
+
+agent.on("agent/message", (payload) => {
+  const { uuid } = payload.agent;
+  const { timestamp } = payload;
+
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent);
+    agentMetrics.set(uuid, {});
+  }
+
+  const metrics = agentMetrics.get(uuid);
+
+  payload.metrics.forEach((m) => {
+    const { type, value } = m;
+
+    if (!Array.isArray(metrics[type])) {
+      metrics[type] = [];
+    }
+
+    const length = metrics[type].length;
+
+    if (length >= 20) {
+      metrics[type].shift();
+    }
+
+    metrics[type].push({
+      value,
+      timestamp: moment(timestamp).format("HH:mm:ss"),
+    });
+  });
+
+  renderData();
+});
+
+tree.on("select", (node) => {
+  const { uuid, type } = node;
+  if (node.agent) {
+    node.extended
+      ? extended.push(uuid)
+      : (extended = extended.filter((e) => e !== uuid));
+    selected.uuid = null;
+    selected.type = null;
+    return;
+  }
+
+  selected.uuid = uuid;
+  selected.type = type;
+
+  renderMetric();
+});
+
 function renderData() {
   const treeData = {};
 
+  let idx = 0;
+
   for (let [uuid, val] of agents) {
-    const title = ` ${val.name} - (${val.pid})`;
+    const title = `${val.name} - (${val.pid})`;
     treeData[title] = {
       uuid,
       agent: true,
+      extended: extended.includes(uuid),
       children: {},
     };
+
+    const metrics = agentMetrics.get(uuid);
+
+    Object.keys(metrics).forEach((type) => {
+      const metric = {
+        uuid,
+        type,
+        metric: true,
+      };
+
+      const metricName = `${type} ${" ".repeat(1000)} ${idx++}}`;
+
+      treeData[title].children[metricName] = metric;
+    });
   }
 
   tree.setData({
@@ -56,7 +140,7 @@ function renderData() {
     children: treeData,
   });
 
-  screen.render();
+  renderMetric();
 }
 
 screen.key(["escape", "q", "C-c"], (ch, key) => {
@@ -65,4 +149,34 @@ screen.key(["escape", "q", "C-c"], (ch, key) => {
 
 agent.connect();
 
+tree.focus();
 screen.render();
+
+function renderMetric() {
+  if (!selected.uuid && !selected.type) {
+    line.setData([
+      {
+        x: [],
+        y: [],
+        title: "",
+      },
+    ]);
+    screen.render();
+    return;
+  }
+
+  const metrics = agentMetrics.get(selected.uuid);
+
+  const values = metrics[selected.type];
+
+  const series = [
+    {
+      title: selected.type,
+      x: values.map((v) => v.timestamp).slice(-10),
+      y: values.map((v) => v.value).slice(-10),
+    },
+  ];
+
+  line.setData(series);
+  screen.render();
+}
